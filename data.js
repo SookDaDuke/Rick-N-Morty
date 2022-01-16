8313b60fb9693b1b9f2b9673bb8473354c2f3443
Jake Sucoff <jsucoff@gmail.com>
2022-01-16 16:19:57 -0500

    rick & morty styles added

--- data.js
+++ data.js
@@ -3,84 +3,84 @@ window.data = {
   totalCPS: 0,
   producers: [
     {
-      id: 'chemex',
+      id: 'plumbus',
       price: 10,
       unlocked: false,
       cps: 1,
       qty: 0
     },
     {
-      id: 'french_press',
+      id: 'mr._meeseeks_box',
       price: 50,
       unlocked: false,
       cps: 2,
       qty: 0
     },
     {
-      id: 'mr._coffee',
+      id: 'jan_michael_vincent',
       price: 100,
       unlocked: false,
       cps: 5,
       qty: 0
     },
     {
-      id: 'ten_cup_urn',
+      id: 'turbulent_juice',
       price: 500,
       unlocked: false,
       cps: 10,
       qty: 0
     },
     {
-      id: 'espresso_machine',
+      id: 'mega_seed',
       price: 1000,
       unlocked: false,
       cps: 20,
       qty: 0
     },
     {
-      id: 'ten_gallon_urn',
+      id: 'microverse_battery',
       price: 5000,
       unlocked: false,
       cps: 50,
       qty: 0
     },
     {
-      id: 'coffeeshop',
+      id: 'space_beth',
       price: 10000,
       unlocked: false,
       cps: 75,
       qty: 0
     },
     {
-      id: 'coffee_factory',
+      id: 'spaceship',
       price: 50000,
       unlocked: false,
       cps: 100,
       qty: 0
     },
     {
-      id: 'coffee_fountain',
+      id: 'ionic_defibulizer',
       price: 100000,
       unlocked: false,
       cps: 200,
       qty: 0
     },
     {
-      id: 'coffee_river',
+      id: 'krombopulos_michael',
       price: 500000,
       unlocked: false,
       cps: 500,
       qty: 0
     },
     {
-      id: 'coffee_ocean',
+      id: 'two_crows',
       price: 1000000,
       unlocked: false,
       cps: 1000,
       qty: 0
     },
     {
-      id: 'coffee_planet',
+      id: 'portal_gun',
       price: 5000000,
       unlocked: false,
       cps: 2000,
