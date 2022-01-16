886a2bd5e943225761dfa41fa1a2af77fe5750e1
Jake <jsucoff@gmail.com>
2022-01-16 16:26:30 -0500

    Added R&M Clicker

---
+++ data.js
@@ -0,0 +1,90 @@
+window.data = {
+  coffee: 0,
+  totalCPS: 0,
+  producers: [
+    {
+      id: 'plumbus',
+      price: 10,
+      unlocked: false,
+      cps: 1,
+      qty: 0
+    },
+    {
+      id: 'mr._meeseeks_box',
+      price: 50,
+      unlocked: false,
+      cps: 2,
+      qty: 0
+    },
+    {
+      id: 'jan_michael_vincent',
+      price: 100,
+      unlocked: false,
+      cps: 5,
+      qty: 0
+    },
+    {
+      id: 'turbulent_juice',
+      price: 500,
+      unlocked: false,
+      cps: 10,
+      qty: 0
+    },
+    {
+      id: 'mega_seed',
+      price: 1000,
+      unlocked: false,
+      cps: 20,
+      qty: 0
+    },
+    {
+      id: 'microverse_battery',
+      price: 5000,
+      unlocked: false,
+      cps: 50,
+      qty: 0
+    },
+    {
+      id: 'space_beth',
+      price: 10000,
+      unlocked: false,
+      cps: 75,
+      qty: 0
+    },
+    {
+      id: 'spaceship',
+      price: 50000,
+      unlocked: false,
+      cps: 100,
+      qty: 0
+    },
+    {
+      id: 'ionic_defibulizer',
+      price: 100000,
+      unlocked: false,
+      cps: 200,
+      qty: 0
+    },
+    {
+      id: 'krombopulos_michael',
+      price: 500000,
+      unlocked: false,
+      cps: 500,
+      qty: 0
+    },
+    {
+      id: 'two_crows',
+      price: 1000000,
+      unlocked: false,
+      cps: 1000,
+      qty: 0
+    },
+    {
+      id: 'portal_gun',
+      price: 5000000,
+      unlocked: false,
+      cps: 2000,
+      qty: 0
+    }
+  ]
+};
